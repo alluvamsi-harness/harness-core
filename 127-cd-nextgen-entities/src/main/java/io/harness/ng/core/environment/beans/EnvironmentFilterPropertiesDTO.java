da9f9f602a92669ff8a2b49aaf304912f8a46d91
Abhinav Hinger <abhinav.hinger@harness.io>
2022-05-18 19:01:34 +0530

    feat: [CDS-37326]: Environment Filters

---
+++ 127-cd-nextgen-entities/src/main/java/io/harness/ng/core/environment/beans/EnvironmentFilterPropertiesDTO.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+package io.harness.ng.core.environment.beans;
+
+import static io.harness.filter.FilterConstants.ENVIRONMENT;
+
+import io.harness.filter.FilterType;
+import io.harness.filter.dto.FilterPropertiesDTO;
+import io.harness.ng.core.common.beans.NGTag;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import io.swagger.annotations.ApiModel;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@ApiModel("EnvironmentFilterProperties")
+@JsonTypeName(ENVIRONMENT)
+public class EnvironmentFilterPropertiesDTO extends FilterPropertiesDTO {
+  private List<String> environmentNames;
+  private List<String> environmentIdentifiers;
+  private String description;
+  private List<NGTag> environmentTags;
+  private List<EnvironmentType> environmentTypes;
+
+  @Override
+  public FilterType getFilterType() {
+    return FilterType.ENVIRONMENT;
+  }
+}
