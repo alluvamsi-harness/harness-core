324096cf638e83431f73844f3c0030072029a96f
Deepak Puthraya <deepak.puthraya@harness.io>
2022-05-19 12:33:10 +0530

    [CDS-37798]: Update build number

--- 800-pipeline-service/src/main/java/io/harness/pms/helpers/PipelineExpressionHelper.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/helpers/PipelineExpressionHelper.java
@@ -8,21 +8,27 @@
 package io.harness.pms.helpers;
 
 import io.harness.PipelineServiceConfiguration;
+import io.harness.account.AccountClient;
 import io.harness.data.structure.EmptyPredicate;
 import io.harness.pms.contracts.ambiance.Ambiance;
 import io.harness.pms.execution.utils.AmbianceUtils;
 import io.harness.pms.pipeline.PipelineEntity;
 import io.harness.pms.pipeline.PipelineEntityUtils;
 import io.harness.pms.pipeline.service.PMSPipelineService;
+import io.harness.remote.client.RestClientUtils;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.net.URL;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Singleton
 public class PipelineExpressionHelper {
   @Inject PMSPipelineService pmsPipelineService;
   @Inject PipelineServiceConfiguration pipelineServiceConfiguration;
+  @Inject private AccountClient accountClient;
 
   public String generateUrl(Ambiance ambiance) {
     String accountId = AmbianceUtils.getAccountId(ambiance);
@@ -37,8 +43,32 @@ public class PipelineExpressionHelper {
     } else if (pipelineEntity.isPresent()) {
       moduleName = PipelineEntityUtils.getModuleNameFromPipelineEntity(pipelineEntity.get(), "cd");
     }
-    return String.format("%s/account/%s/%s/orgs/%s/projects/%s/pipelines/%s/executions/%s/pipeline",
-        pipelineServiceConfiguration.getPipelineServiceBaseUrl(), accountId, moduleName, orgId, projectId,
-        ambiance.getMetadata().getPipelineIdentifier(), ambiance.getPlanExecutionId());
+    String vanityUrl = getVanityUrl(accountId);
+    String baseUrl = getBaseUrl(pipelineServiceConfiguration.getPipelineServiceBaseUrl(), vanityUrl);
+    return String.format("%s/account/%s/%s/orgs/%s/projects/%s/pipelines/%s/executions/%s/pipeline", baseUrl, accountId,
+        moduleName, orgId, projectId, ambiance.getMetadata().getPipelineIdentifier(), planExecutionId);
+  }
+
+  private static String getBaseUrl(String defaultBaseUrl, String vanityUrl) {
+    // e.g Prod Default Base URL - 'https://app.harness.io/ng/#'
+    if (EmptyPredicate.isEmpty(vanityUrl)) {
+      return defaultBaseUrl;
+    }
+    String newBaseUrl = vanityUrl;
+    if (vanityUrl.endsWith("/")) {
+      newBaseUrl = vanityUrl.substring(0, vanityUrl.length() - 1);
+    }
+    try {
+      URL url = new URL(defaultBaseUrl);
+      String hostUrl = String.format("%s://%s", url.getProtocol(), url.getHost());
+      return newBaseUrl + defaultBaseUrl.substring(hostUrl.length());
+    } catch (Exception e) {
+      log.warn("There was error while generating vanity URL", e);
+      return defaultBaseUrl;
+    }
+  }
+
+  private String getVanityUrl(String accountIdentifier) {
+    return RestClientUtils.getResponse(accountClient.getVanityUrl(accountIdentifier));
   }
 }
